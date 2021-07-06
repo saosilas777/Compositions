654345d17e539cff11b75a0a3b02408000fbecd7
Silas Dias <saosilas@hotmail.com>
2021-07-05 21:35:54 -0300

    Add project files.

---
+++ Compositions/Entities/Department.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Compositions.Entities {
+    class Department {
+
+        public string Name { get; set; }
+
+        public Department() { }
+
+        public Department(string name) {
+            Name = name;
+        }
+
+
+    }
+}
---
+++ Compositions/Entities/Worker.cs
@@ -0,0 +1,57 @@
+﻿using Compositions.Entities.Enums;
+using System.Collections.Generic;
+
+
+namespace Compositions.Entities
+{
+    class Worker
+    {
+
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BasySalary { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+        public Worker()
+        {
+
+        }
+
+        public Worker(string name, WorkerLevel level, double basySalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BasySalary = basySalary;
+            Department = department;
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BasySalary;
+            foreach (HourContract contract in Contracts)
+            {
+                if (contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+
+            }
+            return sum;
+
+
+        }
+        
+    }
+    
+}
